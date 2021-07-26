c74efb384a5f5da610448696bdaddba3b591cfc3
Infilit20 <76778693+Infilit20@users.noreply.github.com>
2021-07-26 18:49:18 +0200

    Changes with design, added input form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 const BODY = document.body;
+const Form = document.querySelector('.mainform');
+const SubmitBtn = Form.querySelector('.submitbtn');
+
+SubmitBtn.addEventListener("click", () => {
+    const InputData = document.querySelectorAll("[required]");
+})
